8d8c8c8107aae283b226d449da0d6d198c513857
godcandidate <102991532+godcandidate@users.noreply.github.com>
2024-05-22 10:34:57 +0000

    Create itertools-product.py

---
+++ Python/Itertools/itertools-product.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+print(*product(A,B))
